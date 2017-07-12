699ae911f95be2dacd98edd5cf13b00170908472
Jay Kavathiya <jkavathiya18@gmail.com>
2017-07-11 23:45:20 -0400

    created skeleton of the app

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Jay on 7/11/2017.
+ */
+import {Component} from "@angular/core";
+
+@Component({
+  selector:'app-header',
+  templateUrl:'./header.component.html'
+})
+
+export class HeaderComponent{
+
+}
