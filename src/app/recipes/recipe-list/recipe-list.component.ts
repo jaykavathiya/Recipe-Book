33a89b472c28a17987afae0c7522354200eeed89
Jay Kavathiya <jkavathiya18@gmail.com>
2017-07-12 13:11:40 -0400

    creation of recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes:Recipe[] = [
+    new Recipe('Test Recipe','This is simply test recipe','http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18185711/051107001-01-pumpkin-layer-cake-recipe-main.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Jay on 7/12/2017.
+ */
+export class Recipe{
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imagePath: string){
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+}
